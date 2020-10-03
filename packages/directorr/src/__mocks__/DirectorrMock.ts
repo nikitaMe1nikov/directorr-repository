75d9391a16d73ddf3d6acd8fd98d6769e2ed9695
Nikita Melnikov <nikita.melnikov.fw@gmail.com>
2020-10-03 09:57:41 +0700

    [directorr]: add stack for actions

--- packages/directorr/src/__mocks__/DirectorrMock.ts
+++ packages/directorr/src/__mocks__/DirectorrMock.ts
@@ -1,8 +1,9 @@
 import { getStoreName } from '../utils';
-import { DirectorrInterface } from '../types';
+import { DirectorrInterface, Action } from '../types';
 
 export default class DirectorrMock implements DirectorrInterface {
   stores = new Map();
+  actions: Action[] = [];
   addStores = jest.fn().mockImplementation((...Stores: any[]) => {
     Stores.forEach(Store => {
       const store = new Store();
@@ -23,22 +24,19 @@ export default class DirectorrMock implements DirectorrInterface {
 
     return store;
   });
-  // tempStore: any;
   removeStoreDependency = jest.fn();
   getHydrateStoresState = jest.fn();
   getStore = jest.fn().mockImplementation(Store => this.stores.get(getStoreName(Store)));
   waitAllStoresState = jest.fn().mockImplementationOnce(() => Promise.resolve());
   waitStoresState = jest.fn().mockImplementationOnce(s => Promise.resolve(s));
   findStoreState = jest.fn().mockImplementationOnce(() => Promise.resolve());
-  dispatch = jest.fn();
-  dispatchType = jest.fn();
+  dispatch = jest.fn().mockImplementationOnce(action => this.actions.push(action));
+  dispatchType = jest
+    .fn()
+    .mockImplementationOnce((type, payload) => this.dispatch({ type, payload }));
   addInitState = jest.fn();
   removeStore = jest.fn();
   addReduxMiddlewares = jest.fn();
   addMiddlewares = jest.fn();
   mergeStateToStore = jest.fn();
-
-  // constructor(storeInstance?: any) {
-  //   this.tempStore = storeInstance;
-  // }
 }
