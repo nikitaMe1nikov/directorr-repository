02d433ff4ca7e2b9b78a22de95dc19fee54d7712
Nikita Melnikov <nikita.melnikov.fw@gmail.com>
2020-11-19 01:04:54 +0700

    [directorr-form] export const object

--- packages/directorr-form/src/index.ts
+++ packages/directorr-form/src/index.ts
@@ -3,5 +3,5 @@ import validate from './validate';
 import validateAll from './validateAll';
 export * from './decorators';
 export * from './types';
-
+export { EMPTY_OBJECT, EMPTY_STRING } from '@nimel/directorr';
 export { validate, validateAll };
